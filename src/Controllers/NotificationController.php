76f901942f1f095a0cdd7eae6e537e798be93083
rafazingano <rafazingano@gmail.com>
2020-03-06 14:20:43 -0300

    Update all

---
+++ src/Controllers/NotificationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ConfrariaWeb\Notification\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class NotificationController extends Controller
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function markAsRead()
+    {
+        //
+    }
+}
